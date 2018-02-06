ae2217c82bccf9e70899e7af59303c6d1a987ab9
Dw <lo.dawid@gmail.com>
2018-02-06 23:42:51 +0100

    improving sh exection feedback

--- src/clj_aws/provider/shell.clj
+++ src/clj_aws/provider/shell.clj
@@ -5,14 +5,15 @@
 (defn sh-exec
   "Runs a sh command localy"
   [command]
-  (:out (shell/sh "sh" "-c" command)))
+  (let [result    (shell/sh "sh" "-c" command)
+        exit-code (:exit result)
+        out       (:out result)
+        err       (:err result)]
+    (if (= 0 exit-code)
+      out
+      (throw (Exception. err)))))
 
 (defn exec
   "Executes a command "
   [command]
   (cheshire/parse-string (sh-exec command)))
-
-
-
-
-()
